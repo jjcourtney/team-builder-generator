eba581966c7c888c29a7c1c56586bf62ce703906
jjcourtney <jcourtneywork@gmail.com>
2021-08-14 01:02:52 +0100

    feat: added test for invalid email

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,37 +1,43 @@
-const Employee = require("../lib/Employee")
+const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
     describe("getName", () => {
-        it("should return an name of the Employee", () => {
-            const expected = "Jonathan"
-            const result = new Employee("Jonathan", 1, "asd@asd.com").getName()
+        it("should return the name of the Employee", () => {
+            const expected = "Jonathan";
+            const result = new Employee("Jonathan", 1, "asd@asd.com").getName();
 
-            expect(result).toEqual(expected)
+            expect(result).toEqual(expected);
         })
     });
     describe("getId", () => {
         it("should return an id of the Employee", () => {
-            const expected = 1
-            const result = new Employee("Jonathan", 1, "asd@asd.com").getId()
+            const expected = 1;
+            const result = new Employee("Jonathan", 1, "asd@asd.com").getId();
 
-            expect(result).toEqual(expected)
+            expect(result).toEqual(expected);
         })
     });
     describe("getEmail", () => {
         it("should return an email of the Employee", () => {
-            const expected = "asd@asd.com"
-            const result = new Employee("Jonathan", 1, "asd@asd.com").getEmail()
+            const expected = "asd@asd.com";
+            const result = new Employee("Jonathan", 1, "asd@asd.com").getEmail();
 
-            expect(result).toEqual(expected)
-        })
+            expect(result).toEqual(expected);
+        });
+        it("should return 'Invalid email' if incorrect format inputted into email'", () => {
+            const expected = "Invalid email";
+            const result = new Employee("Jonathan", 1, "asf").getEmail();
+
+            expect(result).toEqual(expected);
+        });
     });
     describe("getRole", () => {
         it("Should return 'Employee'", () => {
             
-            const expected = "Employee"
-            const result = new Employee("Jonathan", 1, "asd@asd.com").getRole()
+            const expected = "Employee";
+            const result = new Employee("Jonathan", 1, "asd@asd.com").getRole();
 
-            expect(result).toEqual(expected)
+            expect(result).toEqual(expected);
         })
     });
 })
