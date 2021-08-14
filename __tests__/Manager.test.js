7e34d8bd83b720424e223ae08549a82792b77926
jjcourtney <jcourtneywork@gmail.com>
2021-08-14 01:36:15 +0100

    feat: added test for officeNumber

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,9 +5,11 @@ describe("Manager", () => {
         it("should have a property officeNumber", () => {
             
             const testManager = new Manager("Jonathan", 1, "asd@asd.com", 5);
-            const hasOfficeNumber = testManager.hasOwnProperty("officeNumber")
+            const hasOfficeNumber = testManager.hasOwnProperty("officeNumber");
+            const officeNumberType = typeof testManager.officeNumber;
 
             expect(hasOfficeNumber).toBe(true);
+            expect(officeNumberType).toBe("number");
         })
     });
     describe("getRole", () => {
