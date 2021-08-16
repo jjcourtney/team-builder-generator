24064d2b6eafef2dba5bfcff19e6ac96e3aec257
jjcourtney <jcourtneywork@gmail.com>
2021-08-16 17:18:39 +0100

    created question generator

---
+++ src/getQuestions.js
@@ -0,0 +1,43 @@
+const employeeQuestions = role => `[
+{   
+    type: 'input',
+    message: 'What is the ${role}'s name?',
+    name: 'name'
+},
+{
+    type: 'input',
+    message: 'What is their id number?',
+    name: 'id'
+},
+{
+    type: 'input',
+    message: 'What is their email address?',
+    name: 'email'
+},
+{
+    type: 'input',
+    message: ${getUniqueQuestion(role)},
+    name: 'uniqueData'
+}]`;
+
+
+const getUniqueQuestion = role => {
+    let roleQuestion = {};
+    switch (role){
+
+        case "Manager" : 
+            roleQuestion = "What is their office number?";
+            break;
+
+        case "Engineer" : 
+            roleQuestion = "What is their github id?";
+            break;
+
+        default:
+            roleQuestion = "What school do they attend";
+            break;
+        
+    }
+}
+
+module.exports = employeeQuestions;
