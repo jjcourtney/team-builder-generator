637fde500e3fb42843344c308d3a58918cf50c7e
jjcourtney <jcourtneywork@gmail.com>
2021-08-16 17:53:30 +0100

    feat: imported writeHTMLToFile

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const inquirer = require("inquirer");
 
-const genHTML = require("./src/generateHTML");
+const generateHTML = require("./src/generateHTML");
+const getQuestions = require("./src/getQuestions");
+const writeHTMLToFile = require("./src/writeHTMLToFile");
+
+const teamMemberArray = []
+
+
+const fs = require('fs');
+
+
+const promptUser = (role) => {
+        return inquirer.prompt(getQuestions(role));
+  };
+
+
+const promptFromDetails = (role) => {
+    return promptUser(role)
+      .then((userInput) => teamMemberArray.push(userInput))
+      .catch((error) => console.error(error));
+  };
+
+const promptForRole = () => {
+    return inquirer.prompt([{   
+        type: 'list',
+        message: 'What is the role of the employee you would like to add?',
+        name: 'role',
+        choices: ["Manager", "Engineer", "Intern"]
+    }])
+  }
+const addTeamMember = () => {
+    if (teamMemberArray.length <= 5){
+        promptForRole()
+        .then(userInput => promptFromDetails(userInput.role))
+        .then(addTeamMember())
+        .catch((error) => console.error(error));
+    } else {
+        generateHTML()
+    }
+}
+
+addTeamMember();
